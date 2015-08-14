3fe235a48924c4b827474cee740d1359534b37f6
SpringStorm5 <springstorm666@gmail.com>
2015-08-14 14:00:16 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '999f505a6e0ce145ee704e95531dba63b67caa032da2c3e613ce0bca396f4d768521d1bc3e5e1dc046a6e799f5fd2143a95d1637a76e38cd0ae3717fd72c1202'
